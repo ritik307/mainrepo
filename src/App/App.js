7b80c083dfd74e4a4da8b3f5fccfc116f384527b
Ritik <ritikpr307@gmail.com>
2021-06-29 13:17:05 +0530

    setup the homepage boilerplate code

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import { Body, Navbar, ContentDiv, Footer } from "./styles";
+const App = () => {
+  return (
+    <Body>
+      <Navbar>
+        <h1>hello</h1>
+      </Navbar>
+      <ContentDiv>
+        
+      </ContentDiv>
+      <Footer>
+        <h1>Footer</h1>
+      </Footer>
+    </Body>
+  );
+};
+export default App;
---
+++ src/App/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+    display: flex;
+    flex-direction: column; 
+    height: 100vh;
+`;
+export const Navbar = styled.nav`
+    height: fit-content;
+    position: fixed;
+    width: 100%;
+    color: white;
+    background-color: black;
+`;
+export const ContentDiv = styled.div`
+    margin : 10vh 2vh;
+    /* height: 100vh; */
+`
+export const Footer = styled.footer`
+    height: fit-content;
+    color: white;
+    background-color: black;
+`;
