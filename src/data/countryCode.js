3da79d239201e23b040824f49b48e1180c519665
Ritik <ritikpr307@gmail.com>
2021-11-16 21:54:16 +0530

    added-flags

--- src/data/countryCode.js
+++ src/data/countryCode.js
@@ -1,83 +1,106 @@
+import ind from '../images/flags/in.webp';
+import id from '../images/flags/id.webp';
+import us from '../images/flags/us.webp';
+import br from '../images/flags/br.webp';
+import be from '../images/flags/be.webp';
+import vn from '../images/flags/vn.webp';
+import bd from '../images/flags/bd.webp';
+import ph from '../images/flags/ph.webp';
+import ch from '../images/flags/ch.webp';
+import gb from '../images/flags/gb.webp';
+import ua from '../images/flags/ua.webp';
+import mx from '../images/flags/mx.webp';
+import tr from '../images/flags/tr.webp';
+import it from '../images/flags/it.webp';
+import sg from '../images/flags/sg.webp';
+import fr from '../images/flags/fr.webp';
+import es from '../images/flags/es.webp';
+import th from '../images/flags/th.webp';
+import np from '../images/flags/np.webp';
+import ru from '../images/flags/ru.webp';
+
+
+
 const data=[
     {
         "country": "India",   
-        "code": "in"
+        "code": ind
     },
     {
         "country": "Indonesia",
-        "code" : "id"
+        "code" : id
     },
     {
         "country": "United States",
-        "code": "us"
+        "code": us
     },
     {
         "country": "Brazil",
-        "code": "br"
+        "code": br
     },
     {
         "country": "Russia",
-        "code": "ru"
+        "code": ru
     },
     {
         "country": "Germany",
-        "code": "be"
+        "code": be
     },
     {
         "country": "Viet Nam",
-        "code": "vn"
+        "code": vn
     },
     {
         "country": "Bangladesh",
-        "code": "bd"
+        "code": bd
     },
     {
         "country": "Philippines",
-        "code": "ph"
+        "code": ph
     },
     {
         "country": "China",
-        "code": "cn"
+        "code": ch
     },
     {
         "country": "United Kingdom",
-        "code": "gb"
+        "code": gb
     },
     {
         "country": "Ukraine",
-        "code": "ua"
+        "code": ua
     },
     {
         "country": "Mexico",
-        "code": "mx"
+        "code": mx
     },
     {
         "country": "Turkey",
-        "code": "tr"
+        "code": tr
     },
     {
         "country": "Italy",
-        "code": "it"
+        "code": it
     },
     {
         "country": "Singapore",
-        "code": "sg"
+        "code": sg
     },
     {
         "country": "France",
-        "code": "fr"
+        "code": fr
     },
     {
         "country": "Spain",
-        "code": "es"
+        "code": es
     },
     {
         "country": "Thailand",
-        "code": "th"
+        "code": th
     },
     {
         "country": "Nepal",
-        "code": "np"
+        "code": np
     }
 ];
 export default data;
